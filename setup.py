dd063cb5a5055472c29aed32735ffad232befa5a
Jonathan YUE CHUN <jonathan.yue-chun@mythalesgroup.io>
2020-10-22 16:44:03 +0200

    removed windows generation & urllib restriction

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ def get_version():
 
 def get_install_requires():
     res = ['elasticsearch>=7.0.0,<8.0.0' ]
-    res.append('urllib3>=1.24.2,<1.25')
     res.append('requests>=2.20.0')
     res.append('boto3>=1.9.142')
     res.append('requests_aws4auth>=0.9')
@@ -34,143 +33,37 @@ def get_install_requires():
     res.append('six>=1.11.0')
     return res
 
-try:
-    ### cx_Freeze ###
-    from cx_Freeze import setup, Executable
-    try:
-        import certifi
-        cert_file = certifi.where()
-    except ImportError:
-        cert_file = ''
-    # Dependencies are automatically detected, but it might need
-    # fine tuning.
-
-
-    base = 'Console'
-
-    icon = None
-    if os.path.exists('Elastic.ico'):
-        icon = 'Elastic.ico'
-
-    curator_exe = Executable(
-        "run_curator.py",
-        base=base,
-        targetName = "curator",
-    )
-    curator_cli_exe = Executable(
-        "run_singleton.py",
-        base=base,
-        targetName = "curator_cli",
-    )
-    repomgr_exe = Executable(
-        "run_es_repo_mgr.py",
-        base=base,
-        targetName = "es_repo_mgr",
-    )
-    buildOptions = dict(
-        packages = [],
-        excludes = [],
-        include_files = [cert_file],
-    )
-
-    if sys.platform == "win32":
-        curator_exe = Executable(
-            "run_curator.py",
-            base=base,
-            targetName = "curator.exe",
-            icon = icon
-        )
-        curator_cli_exe = Executable(
-            "run_singleton.py",
-            base=base,
-            targetName = "curator_cli.exe",
-            icon = icon
-        )
-        repomgr_exe = Executable(
-            "run_es_repo_mgr.py",
-            base=base,
-            targetName = "es_repo_mgr.exe",
-            icon = icon
-        )
-
-        msvcrt = 'vcruntime140.dll'
-        buildOptions = dict(
-            packages = [],
-            excludes = [],
-            include_files = [cert_file, msvcrt],
-            include_msvcr = True, 
-        )
-
-    setup(
-        name = "elasticsearch-curator",
-        version = get_version(),
-        author = "Elastic",
-        author_email = "info@elastic.co",
-        description = "Tending your Elasticsearch indices",
-        long_description=fread('README.rst'),
-        url = "http://github.com/elastic/curator",
-        download_url = "https://github.com/elastic/curator/tarball/v" + get_version(),
-        license = "Apache License, Version 2.0",
-        install_requires = get_install_requires(),
-        setup_requires = get_install_requires(),
-        keywords = "elasticsearch time-series indexed index-expiry",
-        packages = ["curator"],
-        include_package_data=True,
-        entry_points = {
-            "console_scripts" : [
-                "curator = curator.cli:cli",
-                "curator_cli = curator.curator_cli:main",
-                "es_repo_mgr = curator.repomgrcli:repo_mgr_cli",
-            ]
-        },
-        classifiers=[
-            "Intended Audience :: Developers",
-            "Intended Audience :: System Administrators",
-            "License :: OSI Approved :: Apache Software License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-        ],
-        test_suite = "test.run_tests.run_all",
-        tests_require = ["mock", "nose", "coverage", "nosexcover"],
-        options = {"build_exe" : buildOptions},
-        executables = [curator_exe,curator_cli_exe,repomgr_exe]
-    )
-    ### end cx_Freeze ###
-except ImportError:
-    setup(
-        name = "elasticsearch-curator",
-        version = get_version(),
-        author = "Elastic",
-        author_email = "info@elastic.co",
-        description = "Tending your Elasticsearch indices",
-        long_description=fread('README.rst'),
-        url = "http://github.com/elastic/curator",
-        download_url = "https://github.com/elastic/curator/tarball/v" + get_version(),
-        license = "Apache License, Version 2.0",
-        install_requires = get_install_requires(),
-        keywords = "elasticsearch time-series indexed index-expiry",
-        packages = ["curator"],
-        include_package_data=True,
-        entry_points = {
-            "console_scripts" : [
-                "curator = curator.cli:cli",
-                "curator_cli = curator.curator_cli:main",
-                "es_repo_mgr = curator.repomgrcli:repo_mgr_cli",
-            ]
-        },
-        classifiers=[
-            "Intended Audience :: Developers",
-            "Intended Audience :: System Administrators",
-            "License :: OSI Approved :: Apache Software License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-        ],
-        test_suite = "test.run_tests.run_all",
-        tests_require = ["mock", "nose", "coverage", "nosexcover"]
-    )
+setup(
+    name = "elasticsearch-curator",
+    version = get_version(),
+    author = "Elastic",
+    author_email = "info@elastic.co",
+    description = "Tending your Elasticsearch indices",
+    long_description=fread('README.rst'),
+    url = "http://github.com/elastic/curator",
+    download_url = "https://github.com/elastic/curator/tarball/v" + get_version(),
+    license = "Apache License, Version 2.0",
+    install_requires = get_install_requires(),
+    keywords = "elasticsearch time-series indexed index-expiry",
+    packages = ["curator"],
+    include_package_data=True,
+    entry_points = {
+        "console_scripts" : [
+            "curator = curator.cli:cli",
+            "curator_cli = curator.curator_cli:main",
+            "es_repo_mgr = curator.repomgrcli:repo_mgr_cli",
+        ]
+    },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    test_suite = "test.run_tests.run_all",
+    tests_require = ["mock", "nose", "coverage", "nosexcover"]
+)
